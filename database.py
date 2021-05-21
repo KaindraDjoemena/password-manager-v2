beab6d6a6031d0eb2daee6beb934fadfe90996df
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-05-21 13:55:29 +0700

    improved database.py

--- database.py
+++ database.py
@@ -1,20 +1,31 @@
-import sqlite3
+import json
 from prettytable import PrettyTable as pt
+from database_parent import Database
 
 
-class Database:
+
+"""
+Child classes of the parent Database
+
+"""
+
+class Accounts_database(Database):
+    def __init__(self, database_name, table_name):
+        super().__init__(database_name, table_name)
+
+        # Makes a table if havent by looking up the config.json file
+        with open("config.json", "r") as json_file:
+            config_file = json.load(json_file)
+        self.have_created_table = config_file["config"]["have_created_accounts_table"]
+        if not self.have_created_table:
+            self.makeTable()
     
-    # Establish connection to the database
-    def __init__(self):
-        self.connection = sqlite3.connect("data.db")
-        self.cursor = self.connection.cursor()
 
-
-    # Initialize database
-    def initializeDatabase(self):
+    # Make Table
+    def makeTable(self):
 
         # Creates the Tables
-        self.cursor.execute("""CREATE TABLE accounts (
+        self.cursor.execute(f"""CREATE TABLE {self.table_name} (
             username text,
             website_name text,
             website_url test,
@@ -22,19 +33,24 @@ class Database:
             email text
         )""")
 
-        self.cursor.execute("""CREATE TABLE user_data (
-            username text,
-            master_password text,
-            email text
-        )""")
-
         self.commitDatabase()
 
-
-    # Inserts to the accounts table
-    def insertToDatabaseAccounts(self, username, website_name, website_url, password, email):
+        # Changes the config file to have made the accounts table
+        with open("config.json") as json_file:
+            config_file = json.load(json_file)
         
-        self.cursor.execute("""INSERT INTO accounts VALUES (:username, :website_name, :website_url, :password, :email)""", {
+        config_file["config"]["have_created_accounts_table"] = True
+
+        with open("config.json", "w") as json_file:
+            json.dump(config_file, json_file, indent=2)
+
+        self.notifyUser(f"made {self.table_name} table")
+
+
+    # Insert data to table
+    def insertToDatabase(self, username, website_name, website_url, password, email):
+        
+        self.cursor.execute(f"""INSERT INTO {self.table_name} VALUES (:username, :website_name, :website_url, :password, :email)""", {
             "username"    : username,
             "website_name": website_name,
             "website_url" : website_url,
@@ -42,60 +58,100 @@ class Database:
             "email"       : email
         })
 
-        self.notifyUser("inserted data to acccounts table")
-
         self.commitDatabase()
+
+        self.notifyUser(f"inserted data to {self.table_name} table")
     
 
-    # Inserts to the user_data table
-    def insertToDatabaseUserData(self, username, master_password, email):
+    # Delete from database table
+    def deleteFromDatabase(self, command):
 
-        self.cursor.execute("""INSERT INTO user_data VALUES (:username, :master_password, :email)""", {
-            "username"       : username,
-            "master_password": master_password,
-            "email"          : email
-        })
-
-        self.notifyUser("inserted data to user_data table")
+        self.cursor.execute(f"""DELETE from {self.table_name} {command}""")
 
         self.commitDatabase()
 
+        self.notifyUser(f"deleted element from database with: '{command}'")
 
-    # Selects from database tables
-    def selectFromDatabase(self, database, selection="*", command=None):
-
-        if command != None:
-            self.cursor.execute(f"SELECT {selection} FROM {database} {command}")
-        else:
-            self.cursor.execute(f"SELECT {selection} FROM {database}")
-
-        self.commitDatabase()
-    
 
     # Displays data
-    def displayData(self, table_name="accounts"):
-        self.selectFromDatabase(table_name, "rowid, *")
+    def displayData(self):
+        self.selectFromDatabase(self.table_name)
         items = self.cursor.fetchall()
 
-        # Makes a table
+        # Formats data with a table
         table = pt(["ID", "Username", "Website Name", "Website URL", "Password", "Email"])
         for item in items:
             table.add_row([item[i] for i in range(6)])
 
-        print(f"\n{table_name.upper()}") # Prints The Table name
+        print(f"\n{self.table_name.upper()}") # Prints The Table name
         print(table) # Prints the table
 
 
-    # Commit connection
-    def commitDatabase(self):
-        self.connection.commit()
+
+class Master_database(Database):
+    def __init__(self, database_name, table_name):
+        super().__init__(database_name, table_name)
+
+        # Makes a table if havent by looking up the config.json file
+        with open("config.json", "r") as json_file:
+            config_file = json.load(json_file)
+        self.have_created_table = config_file["config"]["have_created_master_table"]
+        if not self.have_created_table:
+            self.makeTable()
 
 
-    # Close connection
-    def closeDatabase(self):
-        self.connection.close()
+    # Make table
+    def makeTable(self):
+        
+        self.cursor.execute(f"""CREATE TABLE {self.table_name} (
+            username text,
+            salt text,
+            master_password_hash text,
+            email text
+        )""")
+
+        self.commitDatabase()
+
+        # Changes the config file to have made the accounts table
+        with open("config.json") as json_file:
+            config_file = json.load(json_file)
+        
+        config_file["config"]["have_created_master_table"] = True
+
+        with open("config.json", "w") as json_file:
+            json.dump(config_file, json_file, indent=2)
+
+        self.notifyUser(f"made {self.table_name} table")
+    
+    
+    # Insert data to table
+    def insertToDatabase(self, username, salt, master_password_hash, email):
+        
+        self.cursor.execute("""INSERT INTO user_data VALUES (:username, :master_password, :email)""", {
+            "username"            : username,
+            "salt"                : salt,
+            "master_password_hash": master_password_hash,
+            "email"               : email
+        })
+
+        self.notifyUser(f"inserted data to {self.table_name} table")
+
+        self.commitDatabase()
 
 
-    # Notifies the user
-    def notifyUser(self, notification):
-        print(f"<{notification}>")
+    # Displays data
+    def displayData(self):
+        self.selectFromDatabase(self.table_name)
+        items = self.cursor.fetchall()
+
+        # Formats data with a table
+        table = pt(["ID", "Username", "Password Salt", "Master Password Hash", "Email"])
+        for item in items:
+            table.add_row([item[i] for i in range(5)])
+
+        print(f"\n{self.table_name.upper()}") # Prints The Table name
+        print(table) # Prints the table
+
+
+if __name__ == "__main__":
+    pass
