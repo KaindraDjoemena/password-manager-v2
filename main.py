c019641ff31297422c82e764008160bf0c8e0055
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-05-21 13:55:02 +0700

    changed main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from commands import Commands
 from program import Program
-from database import Database
 
 
 if __name__ == "__main__":
-    pass
+    program = Program()
+
+    program.mainPage()
