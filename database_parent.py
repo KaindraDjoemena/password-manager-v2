0ae9397fdc86a4152a1e5429265280a7757f3043
Kaindra Djoemena <62385409+KaindraDjoemena@users.noreply.github.com>
2021-05-22 20:00:20 +0700

    encryption

--- database_parent.py
+++ database_parent.py
@@ -1,10 +1,30 @@
+import json
 import sqlite3
+from crypto import Crypto
 
 
 class Database:
     
     # Establish connection to the database
     def __init__(self, database_name, table_name):
+
+        self.crypto = Crypto(15)
+
+        with open("config.json") as json_file:
+            config_file = json.load(json_file)
+        self.have_created_key = config_file["config"]["have_created_key"]
+        
+        # Make key if havent
+        if not self.have_created_key:
+            self.key = self.crypto.makeKey() # encryption key
+
+            with open(".key.json", "w") as json_file:
+                json.dump(self.key, json_file, indent=2)
+
+            with open("config.json", "w") as json_file:
+                config_file["config"]["have_created_key"] = True
+                json.dump(config_file, json_file, indent=2)
+
         self.database_name = database_name
         self.table_name = table_name
         self.connection = sqlite3.connect(f"{database_name}.db")
@@ -27,12 +47,9 @@ class Database:
 
 
     # Selects from database tables
-    def selectFromDatabase(self, table, selection="rowid, *", command=None):
+    def selectFromDatabase(self, table, selection="rowid, *"):
 
-        if command != None:
-            self.cursor.execute(f"SELECT {selection} FROM {table} {command}")
-        else:
-            self.cursor.execute(f"SELECT {selection} FROM {table}")
+        self.cursor.execute(f"SELECT {selection} FROM {table}")
 
         self.commitDatabase()
     
